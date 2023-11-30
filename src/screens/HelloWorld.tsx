376de6073b375feb7288c68bb5d0c78bde68c428
moroshandmitry <moroshandmitry@gmail.com>
2023-11-30 12:51:51 +0200

    master: added alias imports

---
+++ src/screens/HelloWorld.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+
+function HelloWorld(): JSX.Element {
+  return (
+    <View>
+      <Text>HelloWorld</Text>
+    </View>
+  );
+}
+
+export default HelloWorld;
